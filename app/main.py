91504d7624205f13061a31515a12542339080a86
Fernando Cordeiro <fernando_cordeiro@live.com>
2021-11-18 11:26:20 +0000

    👕 LINT: Move post and user to routers

--- app/main.py
+++ app/main.py
@@ -1,123 +1,17 @@
-from fastapi import Depends, FastAPI, Response, status
-from fastapi.exceptions import HTTPException
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import get_db, engine
-
+from fastapi import FastAPI
+from . import models
+from .database import engine
+from .routers import post, user
 
 # Creates tables if they don't exist
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+app.include_router(post.router)
+app.include_router(user.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/posts", response_model=list[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-
-    # With raw SQL (using psycopg2)
-    # cursor.execute("""SELECT * FROM public.posts""")
-    # posts = cursor.fetchall()
-    return posts
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    # With raw SQL (using psycopg2)
-    # # Don't use f-strings, as they would leave us vulnerable to SQL injection
-    # cursor.execute(
-    #     """INSERT INTO public.posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    #     (post.title, post.content, post.published),
-    # )
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    return new_post
-
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    # With raw SQL (using psycopg2)
-    # cursor.execute("""SELECT * FROM public.posts WHERE id = %s""", (str(id),))
-    # post = cursor.fetchone()
-    if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id {id} does not exist",
-        )
-    return post
-
-
-@app.delete("/posts/{id}")
-def delete_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    # With raw SQL (using psycopg2)
-    # cursor.execute("""DELETE FROM public.posts WHERE id = %s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-    if not post.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id: {id} does not exist",
-        )
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(
-    id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)
-):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-
-    # With raw SQL (using psycopg2)
-    # cursor.execute(
-    #     """UPDATE public.posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-    #     (post.title, post.content, post.published, str(id)),
-    # )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-    if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id: {id} does not exist",
-        )
-    post_query.update(updated_post.dict(), synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    user.password = utils.hash(user.password)
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-@app.get("/users/{id}", response_model=schemas.UserOut)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id {id} does not exist",
-        )
-    return user
