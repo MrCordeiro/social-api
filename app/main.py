d1759da5f80a523a6498914ae49b5a91a8c1a54f
Fernando Cordeiro <fernando_cordeiro@live.com>
2021-11-18 11:25:53 +0000

    📦 NEW: Get user

--- app/main.py
+++ app/main.py
@@ -104,10 +104,20 @@ def update_post(
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-
     user.password = utils.hash(user.password)
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {id} does not exist",
+        )
+    return user
